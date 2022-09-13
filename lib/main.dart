6c6b31f06a6876c8d538c6899ae68a8159f6bda7
ekky2412 <ekky.raharjo@gmail.com>
2022-09-13 09:54:55 +0700

    first upload

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+// Contoh pemanggilan class ditempat lain agar bisa digunakan
+import 'modul_2/text_dan_button.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Untuk memanggil nama class pada directory lain, import terlebih dahulu class tersebut diatas
+    // Setelah diimport, panggil nama class tersebut
+    return TextDanButton();
+  }
+}
+
